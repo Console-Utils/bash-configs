476e193cbeebb817faac87114bdb4022daf65213
Emily Seville <emilyseville7cf@gmail.com>
2021-10-22 19:57:32 +1000

    Beautiful warn messages

--- update-configs.sh
+++ update-configs.sh
@@ -3,6 +3,9 @@
 declare -i SUCCESS=0
 declare -i WRONG_INPUT=1
 
+declare bold='\e[1m'
+declare reset='\e[0m'
+
 create_backup() {
   input="$1"
   directory="$2"
@@ -13,12 +16,12 @@ create_backup() {
   new_name="$directory/$input.$suffix"
 
   [[ ! -f $old_name ]] && {
-    echo "No '$input' file found in the '$directory' directory." >&2
+    echo -e "❌ No $bold'$input'$reset file found in the $bold'$directory'$reset directory." >&2
     return "$WRONG_INPUT"
   }
   
   [[ -f $new_name ]] &&
-    echo "'$new_name' file already exists in the '$directory' directory and will be overridden." >&2
+    echo -e "⚠️ $bold'$(basename "$new_name")'$reset backup file already exists in the $bold'$directory'$reset directory and will be overridden." >&2
 
   cp -fT "$old_name" "$new_name"
 }
@@ -29,18 +32,19 @@ dotfiles=(".bashrc"
 
 for f in "${dotfiles[@]}"
 do
-  create_backup "$f" "$HOME"
+  create_backup "$f" "$HOME" || exit
 done
 
 repo_url="https://github.com/Console-Utils/bash-configs.git"
 tmp_folder_name="/tmp/$(date +'%m/%d/%Y')"
 
+echo "⌛Cloning started..."
 git clone "$repo_url" "$tmp_folder_name"
 for f in "$tmp_folder_name/."bash*
 do
   name="$(basename "$f")"
   cp -f "$f" "$HOME/$name"
-  echo "'$name' file updated according to '$repo_url' repo."
+  echo -e "✅$bold'$name'$reset file updated according to $bold'$repo_url'$reset repo."
 done
 
 rm -rf "$tmp_folder_name"
